7a7f54e022c72a7785f2c754d88e1f3c4e8aefca
lemonjai <leocheung_1986@Hotmail.com>
2017-06-24 22:51:31 -0400

    add devise model active admin, student, publisher and developer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :students
+  devise_for :publishers
+  devise_for :developers
+  devise_for :active_admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
