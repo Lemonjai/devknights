f3df31aade3e43ea5ffd5d3041e2c6a8104c7d23
lemonjai <leocheung_1986@Hotmail.com>
2017-06-25 00:27:45 -0400

    update the table view

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -14,4 +14,25 @@ ActiveAdmin.register Page do
 
   permit_params :title, :section_id, :body, :order, :is_published, :menu_displayed
 
+  index do 
+    column :id
+    column :title, :sortable => :title
+    column :section, :sortable => :section
+    column :created_at, :sortable => :created_at
+    column :order
+    actions
+  end
+
+  form do |f|
+    f.inputs "Details" do 
+      f.input :title, :label => "Title"
+      f.input :section, :label => "Section"
+      f.input :body, :label => "Body"
+      f.input :order, :label => "Order"
+      f.input :is_published, :label => "Published"
+      f.input :menu_displayed, :label => "Display in Menu"
+    end
+    f.actions
+  end
+
 end
