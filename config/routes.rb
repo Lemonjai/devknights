67e4e8286e5f1ee19a2519bd775ac2473619d9cf
lemonjai <leocheung_1986@Hotmail.com>
2017-06-24 23:47:38 -0400

    update the security for devise to use activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
 
-  devise_for :developers, ActiveAdmin::Devise.config
-  devise_for :publishers, ActiveAdmin::Devise.config
+  devise_for :developers, ActiveAdmin::Devise.config.merge({path: '/develop'})
+  devise_for :publishers, ActiveAdmin::Devise.config.merge({path: '/publish'})
   devise_for :admin_users, ActiveAdmin::Devise.config
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
